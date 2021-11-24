1d8cccf2b1aea8010cc619b9ab1e5e298c57c956
Ryan Kirsch <agalloch88@gmail.com>
2021-11-24 16:06:29 -0500

    begin authentication implementation, rework error handling into separate function

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=100, default=None, null=True)
+    website = models.CharField(max_length=100, default=None, null=True)
     message = models.CharField(max_length=500, blank=True)
+    owner = models.ForeignKey(User, related_name="leads", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
