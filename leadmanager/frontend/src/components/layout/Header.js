a1add6401448bb2e2e5cff6425d633317c101a61
Ryan Kirsch <agalloch88@gmail.com>
2021-11-26 08:42:33 -0500

    implement Register/Login pages plus forms, add React Router and routes, protect Dashboard route

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Header extends Component {
     render() {
@@ -9,9 +10,11 @@ export class Header extends Component {
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">GiftETrip Lead Manager</a>
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    </ul>
+                        <a className="navbar-brand" href="#">GiftETrip Lead Manager</a>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><Link to='/register' className='nav-link'>Register</Link></li>
+                            <li className="nav-item"><Link to='/login' className='nav-link'>Login</Link></li>
+                        </ul>
                     </div>
                 </div>
             </nav>
