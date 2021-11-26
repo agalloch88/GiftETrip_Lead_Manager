2a8fd3dd4797afd6ec3332423355c6b77ae88c9e
Ryan Kirsch <agalloch88@gmail.com>
2021-11-26 12:18:35 -0500

    finish registration flow, add additional error messages for duplicate username and passwords not matching

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { returnErrors } from './messages';
-import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS } from './types';
+import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 // LOAD USER
 
@@ -46,6 +46,34 @@ export const login = (username, password) => dispatch => {
             dispatch(returnErrors(err.response.data, err.response.status));
             dispatch({ 
                 type: LOGIN_FAIL
+            });
+        });
+}
+
+// REGISTER USER
+
+export const register = ({ username, password, email }) => dispatch => {
+
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Request body
+    const body = JSON.stringify({ username, password, email });
+
+    axios.post('/api/auth/register', body, config)
+        .then(res => {
+            dispatch({ 
+                type: REGISTER_SUCCESS, 
+                payload: res.data
+            });
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ 
+                type: REGISTER_FAIL
             });
         });
 }
